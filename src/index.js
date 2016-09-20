003e5705668edf9ab081364059a0bc663872a7a5
Cichol <cichol@live.cn>
2016-09-20 22:40:30 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -24,15 +24,7 @@ xhr.send()
 
 function init(list){
 	ReactDOM.render(<Player style={{ marginTop: calcMargin(document.body.clientHeight) }}
-		songs={[
-		{
-			cover: "http://p3.music.126.net/b5P2i1-Zzg2bCJpEMQwkoQ==/2412328511400856.jpg?param=130y130",
-			title: "ミラクル・ガール",
-			artist: "Rasmus Faber",
-			url: 'http://7mj4yb.com1.z0.glb.clouddn.com/miracle_girl.mp3',
-			album: "ラスマス・フェイバー・プレゼンツ・プラチナ・ジャズ ～アニメ・スタンダード Vol.4～"
-		}
-	].concat(list)}/>, document.querySelector('#player'))
+		songs={list}/>, document.querySelector('#player'))
 
 	window.onresize = function(){
 		ReactDOM.render(<Player style={{ marginTop: calcMargin(document.body.clientHeight)}}/>, document.querySelector('#player'))
