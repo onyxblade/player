e49de4a8fa3c15b8eb2201dff510e45b6b29a8ef
Cichol <cichol@live.cn>
2016-09-20 20:55:27 +0800

    add api

--- src/index.js
+++ src/index.js
@@ -13,24 +13,27 @@ function calcMargin(height){
 	}
 }
 
-ReactDOM.render(<Player style={{ marginTop: calcMargin(document.body.clientHeight) }} 
-	songs={[
-	{
-		cover: "http://p3.music.126.net/b5P2i1-Zzg2bCJpEMQwkoQ==/2412328511400856.jpg?param=130y130",
-		title: "ミラクル・ガール",
-		artist: "Rasmus Faber",
-		url: 'http://7mj4yb.com1.z0.glb.clouddn.com/miracle_girl.mp3',
-		album: "ラスマス・フェイバー・プレゼンツ・プラチナ・ジャズ ～アニメ・スタンダード Vol.4～"
-	},
-	{
-		cover: "http://img.xiami.net/images/album/img35/10235/533041374829256_2.jpg",
-		title: "Knockin' on Heaven's Door",
-		artist: "Guns N' Roses",
-		url: 'http://m5.file.xiami.com/235/10235/53304/1036009_2461126_l.mp3?auth_key=c686933bcad00feef82aec254fbd3cf3-1461369600-0-null',
-		album: "Use Your Illusion II"
-	},
-]}/>, document.querySelector('#player'))
+var xhr = new XMLHttpRequest()
+xhr.onload = function(e){
+	var list = JSON.parse(e.target.responseText).map(({album, artist, cover, name, url}) => ({album, artist, title: name, cover, url}))
+	init(list)
+}
+xhr.open('GET', 'http://xiami.carp.mopaasapp.com/collection/226294947')
+xhr.send()
 
-window.onresize = function(){
-	ReactDOM.render(<Player style={{ marginTop: calcMargin(document.body.clientHeight)}}/>, document.querySelector('#player'))
+function init(list){
+	ReactDOM.render(<Player style={{ marginTop: calcMargin(document.body.clientHeight) }}
+		songs={[
+		{
+			cover: "http://p3.music.126.net/b5P2i1-Zzg2bCJpEMQwkoQ==/2412328511400856.jpg?param=130y130",
+			title: "ミラクル・ガール",
+			artist: "Rasmus Faber",
+			url: 'http://7mj4yb.com1.z0.glb.clouddn.com/miracle_girl.mp3',
+			album: "ラスマス・フェイバー・プレゼンツ・プラチナ・ジャズ ～アニメ・スタンダード Vol.4～"
+		}
+	].concat(list)}/>, document.querySelector('#player'))
+
+	window.onresize = function(){
+		ReactDOM.render(<Player style={{ marginTop: calcMargin(document.body.clientHeight)}}/>, document.querySelector('#player'))
+	}
 }
